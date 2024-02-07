f7c15015eaaf8d18ef924016d9c349d44a1239b3
Dongjun Kim <lion001@daum.net>
2024-02-07 14:06:23 +0900

    QRCodeScan 기능 추가 중

--- App.tsx
+++ App.tsx
@@ -1,31 +1,40 @@
-import React, {useRef, useState} from 'react';
+import React, { useRef, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, Dimensions, StyleSheet, Text, View, useWindowDimensions } from 'react-native';
+import { SafeAreaView, Dimensions, StyleSheet, Text, View, useWindowDimensions, TouchableOpacity, Modal } from 'react-native';
 import WebView from 'react-native-webview';
+import QRCodeScanner2 from './app/views/QRCodeScanner2'
 
 const windowDimension = Dimensions.get('window');
 const screenDimension = Dimensions.get('screen');
 
 export default function App() {
- 
+
   const ref = useRef(null);
   const [navState, setNavState] = useState();
+  const [isModalVisible, setModalVisible] = useState(false);
   const _onlayout = e => {
     console.log(e.nativeEvent.layout)
     // ref.current.style = {width: `${e.nativeEvent.layout.width}`, height: `${e.nativeEvent.layout.height}`}
   }
   return (
     <SafeAreaView style={styles.container} onLayout={this._onlayout}>
-      <WebView
-        ref={ref}
-        style={styles.webview}
-        source={{uri: 'http://3.39.213.219:8081'}}
-        // source={{uri: 'http://d-dzterp1.koreacentral.cloudapp.azure.com/'}}
-        // scalesPageToFit={true}
-        onNavigationStateChange={e => setNavState(e)}
-      />
+      <TouchableOpacity onPress={() => setModalVisible(true)}>
+        <WebView
+          ref={ref}
+          style={styles.webview}
+          source={{ uri: 'http://3.39.213.219:8081' }}
+          // source={{uri: 'http://d-dzterp1.koreacentral.cloudapp.azure.com/'}}
+          // scalesPageToFit={true}
+          onNavigationStateChange={e => setNavState(e)}
+        />
+      </TouchableOpacity>
+      <Modal animationType="slide" visible={isModalVisible} transparent={true}>
+        <QRCodeScanner2>
+
+        </QRCodeScanner2>
+      </Modal>
     </SafeAreaView>
-   );
+  );
 }
 
 const styles = StyleSheet.create({
---
+++ app/views/QRCodeScanner2.tsx
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, Button } from 'react-native';
+import { CameraView, Camera } from 'expo-camera/next';
+
+export default function QRCodeScanner2() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    const getCameraPermissions = async () => {
+      const {status}  = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+    getCameraPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  const renderCamera = () => {
+    return (
+      <View style={styles.cameraContainer}>
+        <CameraView
+          onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+          barcodeScannerSettings={{barCodeTypes: [ "qr", "pdf417"]}}
+          style={styles.camera}
+        />
+      </View>
+    );
+  }
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Camera permission not granted</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to the Barcode Scanner App!</Text>
+      <Text style={styles.paragraph}>Scan a barcode to start your job.</Text>
+      {renderCamera()}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setScanned(false)}
+        disabled={scanned}
+      >
+        <Text style={styles.buttonText}>Scan QR to Start your job</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  paragraph: {
+    fontSize: 16,
+    marginBottom: 40,
+  },
+  cameraContainer: {
+    width: '80%',
+    aspectRatio: 1,
+    overflow: 'hidden',
+    borderRadius: 10,
+    marginBottom: 40,
+  },
+  camera: {
+    flex: 1,
+  },
+  button: {
+    backgroundColor: 'blue',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
